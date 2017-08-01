fe17d12b2dfdf58edc3bebb7c8563dab8916afef
SKi <vinayski@gmail.com>
2017-07-31 23:49:07 -0400

    fix o notation bug

--- replacex.py
+++ replacex.py
@@ -1,27 +1,23 @@
 """
-Introduction You are given a string composed of only 1s, 0s, and Xs.
 Write a program that will print out every possible combination
-where you replace the X with ​both 0 and 1.
-
-While your program will take longer to run based on the number of possible combinations,
-your program shouldn’t crash (or hang) on an input with many Xs.
-What is the big O notation for your program?
+where you replace X in a given string with ​both 0 and 1.
 """
 
 import math
 import fire
 import numpy as np
 
-def replace(xstring='X1X0XXX'):
+def replace(xstring='X1X0X'):
     """ Replace X with every possible combination where you replace the X with ​both 0 and 1.
 
     Parameters
     ----------
-    xstring : string, (default is 'X0')
+    xstring : string, (default is 'X1X0X')
+    string composed of only 1s, 0s, and Xs.
 
     Returns
     -------
-    Prints a list of possible combinations.
+    Prints a list of possible combinations while replacing X with 1 and 0.
 
     Example
     -------
@@ -34,18 +30,17 @@ def replace(xstring='X1X0XXX'):
     1011000
     1011010
 
-    Usage:   replacex.py -- --help to display usage
-
+    replacex.py -- --help to display usage
     """
-    xnp   = np.array([x for x in xstring])
-    xpos  = np.where(xnp=='X')[0]
-    nofx  = len(xpos)+1
-    bigo  = math.pow(2, nofx) #Big O Notation for this program
+    xnp = np.array([x for x in xstring])
+    xpos = np.where(xnp == 'X')[0]
+    nofx = len(xpos)
+    bigo = math.pow(2, nofx) #Big O Notation for this program
     packst = '{:0>' + str(nofx) + '}' #pad zeros
     for num in range(int(bigo)):
         binarynum = packst.format(bin(num).split('b')[1]) #split binary number in chars
         xnp.flat[xpos] = [binum for binum in binarynum] #assign 1/0 in place of X
-        [print(x,end='') for x in xnp] #print one combination
+        comp = [print(x, end='') for x in xnp] #print one combination
         print() #print newline
     return "done"
 
@@ -53,7 +48,7 @@ def test_replacex():
     """
         @TODO: add more test cases
     """
-    assert replace('10X10X0') == []
+    assert replace('10X10X0') == "done"
 
 
 if __name__ == "__main__":
